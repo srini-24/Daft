5ebbc63af8371f8143f87fe197b76a1c4ab1d633
Srinivas <80765125+srini-24@users.noreply.github.com>
2023-02-17 13:11:08 +0000

    Update Daft.java

--- Daft.java
+++ Daft.java
@@ -13,8 +13,8 @@ public class Daft {
 	public static void main(String[] args) throws InterruptedException{
 		//public boolean status=false; 
 		String url="https://www.daft.ie/";
-		String email="email605@gmail.com";
-		String password="password@123";
+		String email="rswa@gmail.com";
+		String password="Viak@123";
 		By acceptall_loc=By.xpath("//button[contains(text(),'Acc')]");
 		By share_loc=By.xpath("//li[contains(text(),'Share')]");
 		By enter_loc=By.xpath("//input[contains(@aria-labelledby,'search')]");
@@ -44,8 +44,7 @@ public class Daft {
 		driver.findElement(login_btn).click();
 		Thread.sleep(3000);
 		
-		//Add location ,Minimum Rent Price,Max Rent Price,Minimum Beds and Max Beds
-		filters(driver,"Dublin 4", "400", "3000", "1", "2");
+		filters(driver,"Dublin 8", "300", "3000", "1", "3");
 		
 		int getsize=driver.findElements(By.xpath("//ul[contains(@data-testid,'re')]//li")).size();
 		for(int i=1;i<=getsize;i++){
@@ -91,10 +90,13 @@ public class Daft {
 	
 	public static boolean list(WebDriver driver,int list){
 		try{
-//			String email="email@gmail.com";
-//			String phone="+353899999996";
-			String email="email09@gmail.com";
-			String phone="+353899999996";
+//			String email="vin7@gmail.com";
+			String phone="+353893445521";
+//			String phone="+353844444457";
+//			String email="ka@gmail.com";
+//			String email="cufhomes@gmail.com";
+			String email="amr@gmail.com";
+//			String phone="+353844444463";
 			By list_loc=By.xpath("//ul[contains(@data-testid,'re')]//li["+list+"]");
 			By email_loc=By.xpath("//button[contains(@aria-label,'Email')]");
 			By send_btn=By.xpath("//button[contains(@aria-label,'Send')]");
@@ -111,15 +113,17 @@ public class Daft {
 			ScrollUpOrDown(driver, email_loc);
 			driver.findElement(email_loc).click();
 			Thread.sleep(3000);
-			driver.findElement(fullname).sendKeys("XYZ");
+			driver.findElement(fullname).sendKeys("test");
 			Thread.sleep(3000);
 			driver.findElement(youremail_loc).sendKeys(email);
 			Thread.sleep(3000);
 			driver.findElement(phone_loc).sendKeys(phone);
 			Thread.sleep(3000);
 			driver.findElement(msg_loc).clear();
-			driver.findElement(msg_loc).sendKeys("Hello , Me and my partner and more 2 our friends are looking for a permanent accommodation. " + '\n' + " We came across your advertisement and we are very much interested in renting it." + '\n' + " We all are  working in the Dublin. " + '\n' + " Earning more than 80000+ euros combinely yearly and can provide pay slips and references." + '\n' + " We are clean, peaceful, vegetarian, non party and non smokers. " + '\n' + " We don’t have pets with us. We maintain the cleanliness standard of the house. " + '\n' + " I would like to know if you can arange viewing." + '\n' + " We can move in immediately as well. " + '\n' + " Thank you, " + '\n' + " XYZ");
-//			driver.findElement(msg_loc).sendKeys("Hello , Me and my fiancé are looking for a permanent accommodation. " + '\n' + " We came across your advertisement and we are very much interested in renting it. " + '\n' + " We both are full time working professionals earning more than 70000+ euros yearly and can provide pay slips and references. " + '\n' + " We are clean, peaceful, vegetarian, non party and non smokers." + '\n' + " We don’t have pets with us. " + '\n' + " We maintain the cleanliness standard of the house. " + '\n' + " I would like to know if there is any possibility that we can have a viewing. " + '\n' + " We can move in immediately as well.  " + '\n' + " Thank you," + '\n' + " XYZ");
+//			driver.findElement(msg_loc).sendKeys("Hello , Me and my Sister are looking for a permanent accommodation. " + '\n' + " We came across your advertisement and we are very much interested in renting it. " + '\n' + " We are  working in the Dublin.  " + '\n' + " Earning more than 60000+ euros and we have access to funds more than 50000 euros combinely we can provide pay slips and references. " + '\n' + " We are clean, peaceful, non party and non smokers. " + '\n' + " We don’t have pets with us. We maintain the cleanliness standard of the house. " + '\n' + " I would like to know if you can arange viewing.  " + '\n' + " We can move in immediately as well.  " + '\n' + " Thank you,  " + '\n' + " Kavya");
+  //   		driver.findElement(msg_loc).sendKeys("Hello , Me and my partner and more 2 our friends are looking for a permanent accommodation. " + '\n' + " We came across your advertisement and we are very much interested in renting it." + '\n' + " We all are  working in the Dublin. " + '\n' + " Earning more than 80000+ euros combinely yearly and can provide pay slips and references." + '\n' + " We are clean, peaceful, vegetarian, non party and non smokers. " + '\n' + " We don’t have pets with us. We maintain the cleanliness standard of the house. " + '\n' + " I would like to know if you can arange viewing." + '\n' + " We can move in immediately as well. " + '\n' + " Thank you, " + '\n' + " Kajal");
+     		driver.findElement(msg_loc).sendKeys("Hello ,I am looking for a permanent accommodation." + '\n' + " We came across your advertisement and we are very much interested in renting it." + '\n' + " I am working in the Dublin." + '\n' + " Earning more than 30000+ euros and we have access to funds more than 30000 euros," + '\n' + " I can provide pay slips and references." + '\n' + " I am clean, peaceful, non party and non smoker. " + '\n' + "I don’t have pets with us. " + '\n' + "I maintain the cleanliness standard of the house." + '\n' + " I would like to know if you can arange viewing." + '\n' + " I can move in immediately as well. " + '\n' + " Thank you," + '\n' + "Amey");
+//			driver.findElement(msg_loc).sendKeys("Hello , Me and my fiancé are looking for a permanent accommodation. " + '\n' + " We came across your advertisement and we are very much interested in renting it. " + '\n' + " We both are full time working professionals earning more than 70000+ euros yearly and can provide pay slips and references. " + '\n' + " We are clean, peaceful, vegetarian, non party and non smokers." + '\n' + " We don’t have pets with us. " + '\n' + " We maintain the cleanliness standard of the house. " + '\n' + " I would like to know if there is any possibility that we can have a viewing. " + '\n' + " We can move in immediately as well.  " + '\n' + " Thank you," + '\n' + " Swati");
 			Thread.sleep(3000);
 			driver.findElement(send_btn).click();
 			Thread.sleep(3000);
@@ -143,7 +147,7 @@ public class Daft {
 	
 	public static boolean share(WebDriver driver){
 		try{
-			By share_loc=By.xpath("//li[contains(text(),'Rent')]");
+			By share_loc=By.xpath("//li[contains(text(),'Share')]");
 			By enter_loc=By.xpath("//input[contains(@aria-labelledby,'search')]");
 			By list_loc=By.xpath("//ul[contains(@data-testid,'re')]//li[2]");
 			driver.findElement(share_loc).click();
@@ -159,7 +163,7 @@ public class Daft {
 		return true;
 	}
 	
-	public static boolean filters(WebDriver driver,String location,  String maxprice,String minprice,String maxbed,String minbed){
+	public static boolean filters(WebDriver driver,String location,  String minprice,String maxprice,String maxbed,String minbed){
 		try{
 			By share_loc=By.xpath("//li[contains(text(),'Rent')]");
 			By enter_loc=By.xpath("//input[contains(@aria-labelledby,'search')]");
@@ -179,12 +183,12 @@ public class Daft {
 			Thread.sleep(3000);
 			WebElement tstdrpdwn=driver.findElement(By.name("rentalPriceFrom"));
 			Select dropdown=new Select(tstdrpdwn);
-			dropdown.selectByValue(maxprice);
+			dropdown.selectByValue(minprice);
 			Thread.sleep(3000);
 			
 			WebElement tstdrpdwn2=driver.findElement(By.name("rentalPriceTo"));
 			Select dropdown2=new Select(tstdrpdwn2);
-			dropdown2.selectByValue(minprice);
+			dropdown2.selectByValue(maxprice);
 			
 			Thread.sleep(3000);
 			driver.findElement(done_btn).click();
@@ -201,9 +205,10 @@ public class Daft {
 			WebElement tstdrpdwn4=driver.findElement(By.name("numBedsTo"));
 			Select dropdown4=new Select(tstdrpdwn4);
 			dropdown4.selectByValue(minbed);
-			
+			By bedsdone_btn=By.xpath("//div[contains(@data-testid,'numBedsMenu')]//button//span[contains(text(),'Done')]");
 			Thread.sleep(3000);
-			driver.findElement(done_btn).click();
+			
+			driver.findElement(bedsdone_btn).click();
 			
 			
 			
